9b998704b8f6fc68d1e3ba76e43f3d13ad7f64a0
lisapatrikeevna <lisa15.08patrikeevna@gmail.com>
2021-03-11 21:47:47 +0200

    Updates

---
+++ precache-manifest.a542f8ce702411f4e6e5211b1ea6299d.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8666b3aa1fa2d6f7ade3c829724dedff",
+    "url": "/test-smart/index.html"
+  },
+  {
+    "revision": "99dbed9667c48484084f",
+    "url": "/test-smart/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "f5ab51ce699524e68a30",
+    "url": "/test-smart/static/css/main.04572da1.chunk.css"
+  },
+  {
+    "revision": "99dbed9667c48484084f",
+    "url": "/test-smart/static/js/2.f45b3d57.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/test-smart/static/js/2.f45b3d57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5ab51ce699524e68a30",
+    "url": "/test-smart/static/js/main.6e52590a.chunk.js"
+  },
+  {
+    "revision": "44ab0c337df97d6e27ac",
+    "url": "/test-smart/static/js/runtime-main.c27e6d3d.js"
+  },
+  {
+    "revision": "fbfccd8c0c15c13d6bf74c6591c1fcf9",
+    "url": "/test-smart/static/media/AMERICAN.fbfccd8c.jpg"
+  },
+  {
+    "revision": "ec2a6cb860c7d4c1796789ab8ce81e27",
+    "url": "/test-smart/static/media/CHINESE.ec2a6cb8.jpg"
+  },
+  {
+    "revision": "d1608068bba033852a28a9fb449bc3cd",
+    "url": "/test-smart/static/media/FRENCH.d1608068.jpg"
+  },
+  {
+    "revision": "3853e6d8505781fe22f642cc4d3e99f9",
+    "url": "/test-smart/static/media/Green.3853e6d8.png"
+  },
+  {
+    "revision": "cd6bbbe5d1919ea31941965bd44fdd55",
+    "url": "/test-smart/static/media/INDIAN.cd6bbbe5.jpg"
+  },
+  {
+    "revision": "184e710912bffc2059e4d21b42dfab3a",
+    "url": "/test-smart/static/media/ITALIAN.184e7109.jpg"
+  },
+  {
+    "revision": "640d2ef9a00d5c0a4dc52074e2bfd418",
+    "url": "/test-smart/static/media/MEXICAN.640d2ef9.jpg"
+  },
+  {
+    "revision": "874560c3bcd15f307fdd293f933dd03f",
+    "url": "/test-smart/static/media/PIZZA.874560c3.jpg"
+  },
+  {
+    "revision": "e7a1cb77b264a570c66823614454acde",
+    "url": "/test-smart/static/media/SEAFOOD.e7a1cb77.jpg"
+  },
+  {
+    "revision": "a97725510b39045ae2903e14b619d07c",
+    "url": "/test-smart/static/media/STEAKHOUSE.a9772551.jpg"
+  },
+  {
+    "revision": "ad31e45e73c622e31a4a9e1d6679ec39",
+    "url": "/test-smart/static/media/SUSHI.ad31e45e.jpg"
+  },
+  {
+    "revision": "1e5a2a83afc6b3632bced535f3803e58",
+    "url": "/test-smart/static/media/bg2.1e5a2a83.png"
+  },
+  {
+    "revision": "d3ee6e7febc848964d919155661e85ae",
+    "url": "/test-smart/static/media/hh.d3ee6e7f.png"
+  },
+  {
+    "revision": "bcf9a3c82027a1273a892adf8e0ea84e",
+    "url": "/test-smart/static/media/offer.bcf9a3c8.jpg"
+  }
+]);
